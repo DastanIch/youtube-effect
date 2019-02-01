07fcb0507847e9745f91e01c5ee40775d13f2732
DastanInch <dstnsmail@gmail.com>
2019-02-01 21:37:42 +0600

    initialize

---
+++ index.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Youtube effect</title>
+    <link rel="stylesheet" href="/static/css/fonts.css">
+    <link rel="stylesheet" href="/static/styles.css?<?=rand()?>">
+</head>
+<body>
+    <header>
+        <div class="container_width">
+            <ul class="header_menu">
+                <li class="active"><a href="#">программа</a></li>
+                <li><a href="#">спикеры</a></li>
+                <li><a href="#">как добраться</a></li>
+                <li><a href="#">зарегистрироваться</a></li>
+            </ul>
+        </div>
+    </header>
+    <div class="content">
+        <section class="section1">
+            <div class="container_width">
+                <img src="/static/img/logo.svg" class="logo_img" alt="">
+                <h1>14 февраля | 10:00<br>
+                    Smartpoint</h1>
+                <p>YouTube – новое телевидение за меньшие деньги
+                    Обзор лучших кейсов по разработке креатива для YouTube
+                    Как выжимать из рекламы на YouTube максимум</p>
+            </div>
+        </section>
+        <section class="section2">
+            <div class="container_width">
+                <h2>УЗНАТЬ О КОНФЕРЕНЦИИ ЗА НЕСКОЛЬКО СЕКУНД</h2>
+                <div class="video_block">
+                    <div class="video_wrapper">
+
+                    </div>
+                    <div class="video_bar"><div></div></div>
+                </div>
+            </div>
+            <img src="/static/img/regular_figure.svg" alt="" class="section2_1">
+            <img src="/static/img/regular_figure.svg" alt="" class="section2_2">
+            <img src="/static/img/regular_figure.svg" alt="" class="section2_3">
+        </section>
+        <section class="section3">
+            <div class="container_width">
+                <div class="section3_info">
+                    <div class="info_item">
+                        <h3>ДЛЯ КОГО?</h3>
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                    <div class="info_item">
+                        <h3></h3>
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                    <div class="info_item">
+                        <h3></h3>
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section4"></section>
+    </div>
+    <footer>
+        <div class="container_width">
+
+        </div>
+    </footer>
+
+</body>
+</html>
